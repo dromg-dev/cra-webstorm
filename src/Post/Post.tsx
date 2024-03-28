1eb65c2575a73801b7da642919f2332448da9fac
dromg-dev <dr.aka.boss@gmail.com>
2024-03-29 00:40:29 +0300

    commit post unstash

--- src/Post/Post.tsx
+++ src/Post/Post.tsx
@@ -4,6 +4,12 @@ export const Post = () => {
     return(
         <div>
             <h2>Post69</h2>
+
+            <p>111</p>
+            <p>222</p>
+            <p>333</p>
+            <p>333</p>
+
         </div>
     )
 }
