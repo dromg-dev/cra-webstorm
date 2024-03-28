b454c1ea12c6299edb9d781217b53f16ca7dd779
dromg-dev <dr.aka.boss@gmail.com>
2024-03-29 01:14:47 +0300

    commit gist

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+function App(){
+
+      //add sum
+
+      const sum = (a: number, b: number) => {
+        return a +b
+      }
+
+
+      return (
+        <div>
+
+
+        </div>
+      );
+}
+
+export default App;
